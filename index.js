84f6a9e77d7e880f2024fd3adfd833a23855777a
brajendra kumar <brajendrak0068@gmail.com>
2021-02-22 19:53:01 +0530

    tested building apk

--- index.js
+++ index.js
@@ -8,37 +8,4 @@ import React from "react"
 import App from './App';
 import {name as appName} from './app.json';
 
-
-
-class BlueScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.blue} />
-        );
-    }
-}
-
-class RedScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.red} />
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    blue: {
-        backgroundColor: "#0000FF",
-        width: "100%",
-        height: "100%"
-    },
-    red: {
-        backgroundColor: "#FF0000",
-        width: "100%",
-        height: "100%"
-    }
-});
-
-AppRegistry.registerComponent('BlueScreen', () => BlueScreen);
-AppRegistry.registerComponent('RedScreen', () => RedScreen);
 AppRegistry.registerComponent(appName, () => App);
